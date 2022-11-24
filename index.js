c8d8933caefac8a8eaab5c552e21f6ebbb763b94
1Md-Rakibul-Islam <rakibul199tech@gmail.com>
2022-11-24 12:14:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { query } = require('express');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// require('crypto').randomBytes(64).toString('hex)'
+
+
+
+async function run() {
+
+    try{
+
+    }
+
+    finally{
+
+    }
+    
+}
+run().catch(console.log())
+
+
+app.get('/', async(req, res) => {
+    res.send('Server server is running');
+})
+
+app.listen(port, () => {
+    console.log(`Server runnin on: ${port}`);
+})
